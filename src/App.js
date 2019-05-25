71db83151373f1eb22198e2e2140ebc7849816cd
John Wolf <johnwolf.lobo@gmail.com>
2019-05-25 09:08:07 -0700

    Baseline setup w Pages and Components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import About from './pages/AboutPage';
+import Cart from './pages/CartPage';
+import Contact from './pages/ContactPage';
+import Default from './pages/DefaultPage';
+import Home from './pages/HomePage';
+import Products from './pages/ProductsPage';
+import SingleProduct from './pages/SingleProductPage';
+import { Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import Sidecart from './components/Sidecart';
+import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello from Tech Store</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Navbar />
+        <Sidebar />
+        <Sidecart />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/products" exact component={Products} />
+          <Route path="/products/:id" component={SingleProduct} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/ProductsPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function ProductsPage() {
+  return (
+    <>
+      <div>
+        <h1>ProductsPage</h1>
+      </div>
+    </>
+  );
+}
