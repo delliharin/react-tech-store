f2e647efc81463d321b3fae2997ceb71d8136ac9
John Wolf <johnwolf.lobo@gmail.com>
2019-05-31 16:39:24 -0700

    Cart and Product pages completed

---
+++ src/components/CartPage/Cart.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Title from '../Title';
+import CartColumns from './CartColumns';
+import CartList from './CartList';
+import CartTotals from './CartTotals';
+
+export default function Cart() {
+  return (
+    <>
+      <section className="py-5">
+        <div className="container">
+          <Title title="your cart item" center />
+        </div>
+        <CartColumns />
+        <CartList />
+        <CartTotals />
+      </section>
+    </>
+  );
+}
--- src/context/context.js
+++ src/context/context.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { linkData } from './linkData';
 import { socialData } from './socialData';
+import { items } from './productData';
 
 const ProductContext = React.createContext();
 
@@ -8,10 +9,140 @@ class ProductProvider extends Component {
   state = {
     sidebarOpen: false,
     cartOpen: false,
-    cartItems: 0,
     links: linkData,
     socialIcons: socialData,
-    cart: []
+    cart: [],
+    cartItems: 0,
+    cartSubTotal: 0,
+    cartTax: 0,
+    cartTotal: 0,
+    storeProducts: [],
+    filteredProducts: [],
+    featuredProducts: [],
+    singleProduct: {},
+    loading: true,
+    search: '',
+    price: 0,
+    min: 0,
+    max: 0,
+    company: 'all',
+    shipping: false
+  };
+
+  componentDidMount() {
+    this.setProducts(items);
+  }
+
+  setProducts = products => {
+    let storeProducts = products.map(item => {
+      const { id } = item.sys;
+      const image = item.fields.image.fields.file.url;
+      const product = { id, ...item.fields, image };
+      return product;
+    });
+
+    let featuredProducts = storeProducts.filter(item => item.featured === true);
+
+    let maxPrice = Math.max(...storeProducts.map(item => item.price));
+
+    this.setState(
+      {
+        storeProducts,
+        filteredProducts: storeProducts,
+        featuredProducts,
+        cart: this.getStorageCart(),
+        singleProduct: this.getStorageProduct(),
+        loading: false,
+        max: maxPrice
+      },
+      () => {
+        this.addTotals();
+      }
+    );
+  };
+
+  getStorageCart = () => {
+    let cart;
+    if (localStorage.getItem('cart')) {
+      cart = JSON.parse(localStorage.getItem('cart'));
+    } else {
+      cart = [];
+    }
+    return cart;
+  };
+
+  getStorageProduct = () => {
+    return localStorage.getItem('singleProduct')
+      ? JSON.parse(localStorage.getItem('singleProduct'))
+      : {};
+  };
+
+  getTotals = () => {
+    let subTotal = 0;
+    let cartItems = 0;
+    this.state.cart.forEach(item => {
+      subTotal += item.total;
+      cartItems += item.count;
+    });
+    subTotal = parseFloat(subTotal.toFixed(2));
+    let tax = subTotal * 0.075;
+    tax = parseFloat(tax.toFixed(2));
+    let total = subTotal + tax;
+    total = parseFloat(total.toFixed(2));
+    return {
+      cartItems,
+      subTotal,
+      tax,
+      total
+    };
+  };
+
+  addTotals = () => {
+    const totals = this.getTotals();
+    this.setState({
+      cartItems: totals.cartItems,
+      cartSubTotal: totals.subTotal,
+      cartTax: totals.tax,
+      cartTotal: totals.total
+    });
+  };
+
+  syncStorage = () => {
+    localStorage.setItem('cart', JSON.stringify(this.state.cart));
+  };
+
+  addToCart = id => {
+    let tempCart = [...this.state.cart];
+    let tempProducts = [...this.state.storeProducts];
+    let tempItem = tempCart.find(item => item.id === id);
+    if (!tempItem) {
+      tempItem = tempProducts.find(item => item.id === id);
+      let total = tempItem.price;
+      let cartItem = { ...tempItem, count: 1, total };
+      tempCart = [...tempCart, cartItem];
+    } else {
+      tempItem.count++;
+      tempItem.total = tempItem.price * tempItem.count;
+      tempItem.total = parseFloat(tempItem.total.toFixed(2));
+    }
+    this.setState(
+      () => {
+        return { cart: tempCart };
+      },
+      () => {
+        this.addTotals();
+        this.syncStorage();
+        this.openCart();
+      }
+    );
+  };
+  setSingleProduct = id => {
+    let product = this.state.storeProducts.find(item => item.id === id);
+    localStorage.setItem('singleProduct', JSON.stringify(product));
+    this.setState({
+      singleProduct: { ...product },
+      loading: false
+    });
   };
 
   handleSidebar = () => {
@@ -27,6 +158,82 @@ class ProductProvider extends Component {
     this.setState({ cartOpen: true });
   };
 
+  // Cart page functionality
+  increment = id => {
+    let tempCart = [...this.state.cart];
+    const cartItem = tempCart.find(item => item.id === id);
+    cartItem.count++;
+    cartItem.total = cartItem.count * cartItem.price;
+    cartItem.total = parseFloat(cartItem.total.toFixed(2));
+    this.setState(
+      () => {
+        return {
+          cart: [...tempCart]
+        };
+      },
+      () => {
+        this.addTotals();
+        this.syncStorage();
+      }
+    );
+  };
+
+  decrement = id => {
+    let tempCart = [...this.state.cart];
+    const cartItem = tempCart.find(item => item.id === id);
+    cartItem.count--;
+    if (cartItem.count === 0) {
+      this.removeItem(id);
+    } else {
+      cartItem.total = cartItem.count * cartItem.price;
+      cartItem.total = parseFloat(cartItem.total.toFixed(2));
+
+      this.setState(
+        () => {
+          return {
+            cart: [...tempCart]
+          };
+        },
+        () => {
+          this.addTotals();
+          this.syncStorage();
+        }
+      );
+    }
+  };
+
+  removeItem = id => {
+    let tempCart = [...this.state.cart];
+    tempCart = tempCart.filter(item => item.id !== id);
+    this.setState(
+      {
+        cart: [...tempCart]
+      },
+      () => {
+        this.addTotals();
+        this.syncStorage();
+      }
+    );
+  };
+
+  clearCart = () => {
+    this.setState(
+      {
+        cart: []
+      },
+      () => {
+        this.addTotals();
+        this.syncStorage();
+      }
+    );
+  };
+
+  handleChange = event => {
+    console.log(event);
+  };
+
+  sortData = () => {};
+
   render() {
     return (
       <ProductContext.Provider
@@ -35,7 +242,14 @@ class ProductProvider extends Component {
           handleSidebar: this.handleSidebar,
           handleCart: this.handleCart,
           closeCart: this.closeCart,
-          openCart: this.openCart
+          openCart: this.openCart,
+          addToCart: this.addToCart,
+          setSingleProduct: this.setSingleProduct,
+          increment: this.increment,
+          decrement: this.decrement,
+          removeItem: this.removeItem,
+          clearCart: this.clearCart,
+          handleChange: this.handleChange
         }}
       >
         {this.props.children}
